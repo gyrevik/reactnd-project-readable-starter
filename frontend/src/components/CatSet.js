3327edbb196c4b16a8c1967dfeafdc91acf76c61
Alex Smotritsky <alex.smotritsky@gmail.com>
2017-10-13 15:48:06 -0400

    feat: added voteComment component, deleted Categories component as it wasn't being used, cleaned up CatSet slightly

--- frontend/src/components/CatSet.js
+++ frontend/src/components/CatSet.js
@@ -5,10 +5,6 @@ export default function CatSet({ cats, setPostCat, clearPostCat, selectedCat }) 
   const spanNormal = { fontWeight:'normal' };
   console.log('cats in CategorySet: ', cats);
   console.log('CategorySet typeof(cats): ', typeof(cats));
-  //cats.categories.map((cat, i) => console.log('map arrow function in CategoryList: ', cat.name));
-  
-  //const catsParsed = JSON.parse(cats);
-  //console.log('catsParsed.categories.length: ', catsParsed.categories.length);
   
   return (
     <span>Post Category:&nbsp;
