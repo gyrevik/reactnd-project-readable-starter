71a3610024e98b949d99c5dac7ffd66dbd33bc20
Alex Smotritsky <alex.smotritsky@gmail.com>
2017-11-06 18:32:10 -0500

    feat: changed from switch to if in simple reducer as per code review

--- frontend/src/reducers/mode.js
+++ frontend/src/reducers/mode.js
@@ -1,10 +1,8 @@
 import { SET_MODE } from '../actions/types';
 
 export default function mode (state = 'none', action) {
-  switch (action.type) {
-    case SET_MODE:
-      return action.mode;
-    default:
-      return state;
-  }
+  if (action.type === SET_MODE)
+    return action.mode;
+  else
+    return state;
 }
